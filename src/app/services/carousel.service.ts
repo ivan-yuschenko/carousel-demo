4cf0ce088434b8b64869004332632e874590048c
ivan-yuschenko <ivan.yuschenko@gmail.com>
2024-02-18 13:38:17 +0200

    Final touches

--- src/app/services/carousel.service.ts
+++ src/app/services/carousel.service.ts
@@ -11,6 +11,6 @@ export class CarouselService {
   constructor(private http: HttpClient) { }
 
   getCarouselItems(): Observable<CarouselItem[]> {
-    return this.http.get<CarouselItem[]>('https://run.mocky.io/v3/3e7485ee-6c5e-46b2-813c-8da70a0eda32');
+    return this.http.get<CarouselItem[]>('https://run.mocky.io/v3/5d285185-fa57-4b0e-b054-616dfd01bd76');
   }
 }
